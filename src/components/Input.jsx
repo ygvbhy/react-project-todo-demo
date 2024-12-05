47194a13f023c6922b90b5af91f5bd8d5726753b
sypark <sangyune.park@k-futuretech.com>
2024-12-05 15:21:11 +0900

    create Project design

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 
 const Input = ({ textarea, label, ...props }) => {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
   return (
-    <p>
-      <label htmlFor={props.id}>{label}</label>
-      {textarea ? <textarea {...props} /> : <input type="text" {...props} />}
+    <p className="flex flex-col gap-1 my-4">
+      <label
+        htmlFor={props.id}
+        className="text-sm font-bold uppercase text-stone-500"
+      >
+        {label}
+      </label>
+      {textarea ? (
+        <textarea className={classes} {...props} />
+      ) : (
+        <input type="text" {...props} className={classes} />
+      )}
     </p>
   );
 };
