5e0ebdd1917ccefb7ff4d6a5cf60ddc0c445ee86
sypark <sangyune.park@k-futuretech.com>
2024-12-05 15:29:24 +0900

    add Button Components and NoProjectSelected page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ProjectsSidebar from "./components/ProjectsSidebar";
-import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+// import NewProject from "./components/NewProject";
 
 function App() {
   return (
     <main className="h-screen  my-8 flex gap-8 ">
       <ProjectsSidebar />
-      <NewProject />
+      {/* <NewProject /> */}
+      <NoProjectSelected />
     </main>
   );
 }
